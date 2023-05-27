d9a6c5966eb39df2f6a28e6bd38150d479406766
B1MB0N <116427526+B1MB0N@users.noreply.github.com>
2023-05-27 16:01:16 +0700

    Create buoi_1.py

---
+++ Buoi_1/buoi_1.py
@@ -0,0 +1,14 @@
+# Bang cuu chuong 2
+print("2 x 1 =", 2 * 1)
+print("2 x 2 =", 2 * 2)
+print("2 x 3 =", 2 * 3)
+print("2 x 4 =", 2 * 4)
+print("2 x 5 =", 2 * 5)
+print("2 x 6 =", 2 * 6)
+print("2 x 7 =", 2 * 7)
+print("2 x 8 =", 2 * 8)
+print("2 x 9 =", 2 * 9)
+print("2 x 10 =", 2 * 10)
+# Bang cuu chuong 3
+print("3 x 1 =", 3 * 1)
+print("3 x 2 =", 3 * 1)
